d37d47a040e9254d0e806aef5803c2516b1f2934
satyaprakashnayak <satyaprakash.nayak@gmail.com>
2015-02-21 23:44:58 -0500

    just adding a new file
    Adding a new file - to the test repo

---
+++ test.py
@@ -0,0 +1,15 @@
+# CodeSkulptor runs Python programs in your browser.
+# Click the upper left button to run this simple demo.
+
+# CodeSkulptor runs in Chrome 18+, Firefox 11+, and Safari 6+.
+# Some features may work in other browsers, but do not expect
+# full functionality.  It does NOT run in Internet Explorer.
+
+#from numpy import *
+from math import *
+
+#print matrix('1 3 4; 5 6 9') # matrix is synonymous with mat
+
+print sqrt(2)
+print "test"
+
